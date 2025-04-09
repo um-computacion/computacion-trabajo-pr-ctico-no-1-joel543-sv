11162537ae6d0e5683bb61290ceb3b82b4ccec22
joel543-sv <156801264+joel543-sv@users.noreply.github.com>
2025-04-09 12:43:46 -0300

    Add files via upload

---
+++ roman_converter.py
@@ -0,0 +1,17 @@
+# joel reynoso
+
+def decimal_to_roman(number):
+    if number <= 0 or number > 3999:
+        raise ValueError("El número debe estar entre 1 y 3999")
+    
+    roman_numerals = {
+        1000: "M", 900: "CM", 500: "D", 400: "CD",
+        100: "C", 90: "XC", 50: "L", 40: "XL",
+        10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"
+    }
+    result = ""
+    for value, symbol in roman_numerals.items():
+        while number >= value:
+            result += symbol
+            number -= value
+    return result
